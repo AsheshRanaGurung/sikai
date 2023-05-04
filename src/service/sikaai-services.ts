b72f5cff2df1870b04f56d7b751da56d28444426
jenishakhaigulicf <jenisha.khaiguli@cloudfactory.com>
2023-05-04 13:41:40 +0545

    IT module addition

--- src/service/sikaai-services.ts
+++ src/service/sikaai-services.ts
@@ -12,6 +12,22 @@ export interface IServiceResponse extends IServiceReq {
   id: string;
 }
 
+export interface ICourseResponse {
+  id: number;
+  name: string;
+  description: string;
+  service: number;
+  course_info: CourseInfo;
+  is_active: boolean;
+  created_at: string;
+}
+
+export interface CourseInfo {
+  deduction_mark: string;
+  time_limit: string;
+  total_questions: number;
+}
+
 const getServices = () => {
   return httpClient.get<SikaaiResponse<IServiceResponse[]>>(api.service.get);
 };
@@ -61,4 +77,30 @@ const useUpdateServices = () => {
   });
 };
 
-export { useGetServices, useUpdateServices, useGetServiceById };
+// course = subject in UI
+const getServiceCourse = (serviceId: string) => {
+  console.log("i was here");
+  return httpClient.get<SikaaiResponse<ICourseResponse[]>>(
+    api.service.course.get.replace("{service_id}", serviceId)
+  );
+};
+
+const useGetServiceCourse = ({ serviceId }: { serviceId: string }) => {
+  return useQuery(
+    [api.service.course.get, serviceId],
+    () => getServiceCourse(serviceId),
+    {
+      enabled: !!serviceId,
+      select: ({ data }) => data?.data,
+      onError: () => {
+        toastFail("Subjects couldnot be fetched");
+      },
+    }
+  );
+};
+export {
+  useGetServices,
+  useUpdateServices,
+  useGetServiceById,
+  useGetServiceCourse,
+};
