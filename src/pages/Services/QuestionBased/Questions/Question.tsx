3afba26650fac0d39959e788b9b75452c6818128
akashkhadka099 <akashkhadka099@gmail.com>
2023-05-22 18:02:41 +0545

    fix: fixed question model set questions create

--- src/pages/Services/QuestionBased/Questions/Question.tsx
+++ src/pages/Services/QuestionBased/Questions/Question.tsx
@@ -29,21 +29,22 @@ import httpStatus from "http-status";
 import { NAVIGATION_ROUTES } from "@sikaai/routes/routes.constant";
 import { AddImageIcon } from "@sikaai/assets/svgs/index";
 import { useState } from "react";
+import { convertToBase64 } from "@sikaai/utils/index";
 
 const defaultValues = {
   question_text: "",
   // question_image: "",
   subject_question_set_id: null as number | null,
-  answer_text1: "",
-  answer_text2: "",
-  answer_text3: "",
-  answer_text4: "",
+  answer_text1: null,
+  answer_text2: null,
+  answer_text3: null,
+  answer_text4: null,
   optionAImage: null,
   optionBImage: null,
   optionCImage: null,
   optionDImage: null,
   answer: "",
-  description: "",
+  description: null,
   image: null,
 };
 
@@ -61,35 +62,43 @@ function MyComponent() {
     onStatusOpen();
   };
 
-  const onSubmitHandler = async (questionDetails: any) => {
+  const onSubmitHandler = async (questionDetails: typeof defaultValues) => {
     const requestBody = {
       question_text: questionDetails?.question_text,
       subject_question_set_id: +questionSetId ?? 0,
       options: [
         {
           answer_text: questionDetails?.answer_text1,
-          // answer_image: questionDetails?.optionAImage[0],
+          answer_image_base64: await convertToBase64(
+            questionDetails?.optionAImage?.[0]
+          ),
           is_correct: questionDetails?.answer === "A" ? true : false,
         },
         {
           answer_text: questionDetails?.answer_text2,
-          // answer_image: questionDetails?.optionBImage[0],
+          answer_image_base64: await convertToBase64(
+            questionDetails?.optionBImage?.[0]
+          ),
           is_correct: questionDetails?.answer === "B" ? true : false,
         },
         {
           answer_text: questionDetails?.answer_text3,
-          // answer_image: questionDetails?.optionCImage[0],
+          answer_image_base64: await convertToBase64(
+            questionDetails?.optionCImage?.[0]
+          ),
           is_correct: questionDetails?.answer === "C" ? true : false,
         },
         {
           answer_text: questionDetails?.answer_text4,
-          // answer_image: questionDetails?.optionDImage[0],
+          answer_image_base64: await convertToBase64(
+            questionDetails?.optionDImage?.[0]
+          ),
           is_correct: questionDetails?.answer === "D" ? true : false,
         },
       ],
       solution: {
         description: questionDetails?.description,
-        // image: questionDetails?.image[0],
+        image: await convertToBase64(questionDetails?.image?.[0]),
       },
     };
 
